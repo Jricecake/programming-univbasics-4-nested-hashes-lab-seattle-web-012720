fb5cb0c6687d29de8ef7c722023cdc2494ebb937
Jonathan Riecke <jonathanriecke@gmail.com>
2020-01-13 19:18:18 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -16,10 +16,13 @@ def hopper
         }
      }
 
+return programmer_hash[:grace_hopper]
 
 end
 
 def alan_kay_is_known_for
+  return programmer_hash[:alan_kay][:known_for]
+
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
 	
 	programmer_hash = 
@@ -59,10 +62,11 @@ end
 
 def adding_matz
 # add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
+programmer_hash[:yukihiro_matsumoto] = {
+    :known_for => "Ruby",
+    :languages => ["LISP", "C"]
+ }
+ 
 
 	programmer_hash = 
  		{
@@ -101,7 +105,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
+  programmer_hash[:alan_kay][:known_for] = [:alans_new_info]
      
 end
 
